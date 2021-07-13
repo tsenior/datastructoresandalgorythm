ee5185218dd6ff38599ac6e8c4a7f7c4940f7c8d
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-07-14 01:57:46 +0200

    linked list

---
+++ src/com/tinyiko/LinkedList.java
@@ -0,0 +1,32 @@
+package com.tinyiko;
+
+import org.w3c.dom.Node;
+
+public class LinkedList {
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+    private Node first;
+    private Node last;
+
+
+
+    public void addLast(int item){
+        var node = new Node(item);
+        if (first == null)
+            first = last = node;
+        else{
+            last.next = node;
+            last = node;
+        }
+    }
+    
+}
+
