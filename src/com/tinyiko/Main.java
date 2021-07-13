a4eae0c98bdcdb82ca4010ec7e56dd2fc2774423
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-07-14 00:37:06 +0200

    linked list implementation

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -1,18 +1,36 @@
 package com.tinyiko;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.addFirst(5);
+        linkedList.add(10);
+        linkedList.add(20);
+        linkedList.add(20);
+        linkedList.add(20);
+        linkedList.add(50);
+        linkedList.add(60);
+        linkedList.add(70);
+        linkedList.addLast(75);
 
-        Array array = new Array(3);
-        array.insert(10);
-        array.insert(20);
-        array.insert(30);
-        array.insert(40);
-        System.out.println(array.indexOf(50));
-        array.printer();
+
+        System.out.println("the linkedList has "+linkedList.size()+ " number of items");
+        linkedList.remove(0);
+        System.out.println("index of 50 "+linkedList.indexOf(50));
+        System.out.println("last index of 20 "+linkedList.lastIndexOf(20));
+        System.out.println("linkedList contains 70, true of false = "+ linkedList.contains(70));
+        System.out.println("afer removing first index, the linkedList has "+linkedList.size()+ " number of items");
+        System.out.println("linkedList contains " + linkedList);
+
+        System.out.println("CONVERTING A LINKEDLIST TO A REGULAR ARRAY");
+        Object[] array = linkedList.toArray();
+        System.out.println("regular array contains " + Arrays.toString(array));
 
     }
 }
